9c4d01134bdedf7dffcaf8338c465363167323ce
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2023-02-08 22:35:14 -0300

    feat: bounding boxes, road mask

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import cv2 as cv
+import numpy as np
+
+BOX_COLOR = (0,255,0)
+BOX_THICKNESS = 2
 
 class Camera:
+
+    def __init__(self):
+       self.ROAD_MASK = cv.imread("RoadMask.jpg")[:,:,0]
     #  Infomrações uteis a coloca no init
     #  1) angulação da câmera com a pista
     #  2) Máscara para remover "o que não é pista"
@@ -8,28 +15,36 @@ class Camera:
 
     def run(self,capture:cv.VideoCapture):
         backgtroundSubtractor =  cv.bgsegm.createBackgroundSubtractorMOG()
-        
+        counter=0
         while(capture.isOpened()):
+            counter+=1
             ret,frame = capture.read()
 
             if not ret:
                 raise  Exception("Erro! Vídeo não pôde ser carregado apropriadamente.") 
 
             grayFrame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-            blurredFrame = cv.GaussianBlur(frame,(3,3),5)  # Testar outro filtros (se fizer diferença no resultado final)
+            blurredFrame = cv.GaussianBlur(grayFrame,(3,3),5)  # Testar outro filtros (se fizer diferença no resultado final)
             noBGFrame = backgtroundSubtractor.apply(blurredFrame) 
-
+            np.bitwise_and(noBGFrame,self.ROAD_MASK,noBGFrame)
             kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(8,8))
             noBGFrame = cv.morphologyEx(noBGFrame,cv.MORPH_CLOSE,kernel) # Vale a penna fazer outro fechamento?
             noBGFrame = cv.morphologyEx(noBGFrame,cv.MORPH_CLOSE,kernel) 
 
-            bounding, img = cv.findContours(noBGFrame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # Testar outros retrival modes
+            contours, hierarchy = cv.findContours(noBGFrame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # Testar outros retrival modes
 
-            cv.imshow("nbg",noBGFrame)
-            cv.waitKey(100)
+            for objectCoords in contours:
+                # Vale à pena validar tamanhos dos contornos?
+                x,y,w,h = cv.boundingRect(objectCoords)
+                frame = cv.rectangle(frame,(x,y),(x+w,y+h),BOX_COLOR,BOX_THICKNESS)
+            
+            cv.imshow("Video:",frame)
 
+            if(counter==20):
+                cv.imwrite("VAI.jpg",frame)
             if cv.waitKey(1) == ord('q'):   
                 break
+
 
         capture.release()
         cv.destroyAllWindows()
